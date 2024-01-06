1b33b03db12234e18abedfb12756e5eed6ed8a26
jayuchawla78 <jayprakashchawla78@gmail.com>
2024-01-07 00:33:58 +0530

    adding logic to view specific post

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', view=views.post_list, name='post_list'),
+    path(route='', view=views.post_list, name='post_list'),
+    path(route='post/<int:pk>/', view=views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request=request, template_name='blog/post_list.html', context={})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request=request, 
+        template_name='blog/post_list.html', 
+        context={'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request=request, template_name='blog/post_detail.html', context={'post': post})
