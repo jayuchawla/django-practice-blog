eeaa4375af7537d809e42397ee4bdb403f3c7ded
jayuchawla78 <jayprakashchawla78@gmail.com>
2024-01-07 14:24:27 +0530

    adding form to create and edit post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from .import views
 
 urlpatterns = [
     path(route='', view=views.post_list, name='post_list'),
-    path(route='post/<int:pk>/', view=views.post_detail, name='post_detail')
+    path(route='post/<int:pk>/', view=views.post_detail, name='post_detail'),
+    path(route='post/new/', view=views.post_new, name='post_new'),
+    path(route='post/<int:pk>/edit/', view=views.post_edit, name='post_edit'),
 ]
